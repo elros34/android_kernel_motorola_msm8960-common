00ea4b12628f2aee4607eed3550eca4d11c56d87
Hans Schillstrom <hans.schillstrom@ericsson.com>
2012-04-23 03:35:26 +0000

    netfilter: ip6_tables: add flags parameter to ipv6_find_hdr()
    This patch adds the flags parameter to ipv6_find_hdr. This flags
    allows us to:
    
    * know if this is a fragment.
    * stop at the AH header, so the information contained in that header
      can be used for some specific packet handling.
    
    This patch also adds the offset parameter for inspection of one
    inner IPv6 header that is contained in error messages.
    
    Change-Id: I8aa13399597dcb72c73084bcd7f8ca4156326357
    Signed-off-by: Hans Schillstrom <hans.schillstrom@ericsson.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- include/linux/netfilter_ipv6/ip6_tables.h
+++ include/linux/netfilter_ipv6/ip6_tables.h
@@ -288,9 +288,15 @@ extern unsigned int ip6t_do_table(struct sk_buff *skb,
 
 /* Check for an extension */
 extern int ip6t_ext_hdr(u8 nexthdr);
+
+enum {
+	IP6T_FH_F_FRAG	= (1 << 0),
+	IP6T_FH_F_AUTH	= (1 << 1),
+};
+
 /* find specified header and get offset to it */
 extern int ipv6_find_hdr(const struct sk_buff *skb, unsigned int *offset,
-			 int target, unsigned short *fragoff);
+			 int target, unsigned short *fragoff, int *fragflg);
 
 #ifdef CONFIG_COMPAT
 #include <net/compat.h>
